328636ca5596c4760b7424e0f491669af4441618
Jonathan Eads <superjeads@gmail.com>
2013-09-11 09:52:34 -0700

    integrated alert tables into perftest schema

---
+++ datazilla/model/alerts.py
@@ -0,0 +1,19 @@
+import sys
+import time
+
+from numpy import mean, std, isnan, nan
+
+from django.conf import settings
+
+from base import DatazillaModelBase
+
+class AlertsModel(DatazillaModelBase):
+    """
+    Public interface to all alert relevant data in the schema.
+    """
+
+    def __init__(self, project=None, metrics=()):
+
+        super(AlertsModel, self).__init__(project)
+
+
